73497c8c6f589324d8e5dee28d2b04ae49975c19
ayu-tw <80678206+ayu-tw@users.noreply.github.com>
2021-12-13 13:12:44 +0100

    rendered the word meanings

---
+++ src/Meaning.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definitions, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definitions.definition}
+              <br /> {definitions.example}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/Result.js
@@ -0,0 +1,22 @@
+import react from "react";
+import Meaning from "./Meaning";
+
+export default function Result(props) {
+  console.log(props.result);
+  if (props.result) {
+    return (
+      <div className="Result">
+        <h2>{props.result.word}</h2>
+        {props.result.meanings.map(function (meanings, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meanings} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
